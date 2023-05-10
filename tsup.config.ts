d87f22d640fc870043ad75acae29532afe86149c
DreamOfIce <dreamofice@outlook.com>
2023-05-10 23:58:03 +0800

    chore(build): migrate from vite to tsup
    Reasons:
    - Native support for sourcemap
    - Vite will incorrectly export `useI18nData` in `node/index`
    - Faster builds (mainly in dts generation)

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import vuePlugin from "esbuild-plugin-vue3";
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    "node/index": "src/node/index.ts",
+    "client/index": "src/client/index.ts",
+    "client/config": "src/client/config.ts",
+  },
+  target: "node14",
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  external: [
+    "chokidar",
+    "vue",
+    /vuepress-share(\/.+)?/,
+    /@vuepress\/.+/,
+    /@temp\/.+/,
+  ],
+  esbuildPlugins: [vuePlugin()],
+});
